4223216fc4cbe79254142fb2410bec4e95d54930
Dr Masroor Ehsan <masroore@gmail.com>
2026-02-26 20:12:51 +0600

    Enhance logger configuration by adding PositionalArgumentsFormatter, format_exc_info, and UnicodeDecoder processors

--- scraper/logger.py
+++ scraper/logger.py
@@ -16,8 +16,11 @@ def _configure() -> structlog.stdlib.BoundLogger:
         structlog.stdlib.filter_by_level,
         structlog.stdlib.add_log_level,
         structlog.stdlib.add_logger_name,
+        structlog.stdlib.PositionalArgumentsFormatter(),
         structlog.processors.TimeStamper(fmt="iso"),
         structlog.processors.StackInfoRenderer(),
+        structlog.processors.format_exc_info,
+        structlog.processors.UnicodeDecoder(),
     ]
 
     if sys.stderr.isatty():
